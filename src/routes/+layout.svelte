308aa11d80ec35f1d56bb994553da9a09fe9460e
Ankit Pandit(Consultant) <Ankit.Pandit@ril.com>
2023-01-20 22:17:52 +0530

    Tailwind added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Header from './Header.svelte';
+	import '../app.postcss';
+	import './styles.css';
+</script>
+
+<div class="flex flex-col min-h-screen">
+	<Header />
+
+	<main class="flex flex-1 flex-col p-4 w-full max-w-5xl mx-auto mt-0">
+		<slot />
+	</main>
+
+	<footer class="flex flex-col justify-center items-center p-3">
+		<p>
+			visit <a class="font-bold" href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+		</p>
+	</footer>
+</div>
+
+<style>
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Counter from './Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>SvelteKit app</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
